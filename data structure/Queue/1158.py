510d3ca5c2ce8da5baf902f43caf12fd33c9ae15
seon54 <seonhyeongpark54@gmail.com>
2019-09-19 09:31:21 +0900

    adda 1158

---
+++ data structure/Queue/1158.py
@@ -0,0 +1,44 @@
+"""
+조세퍼스 문제는 다음과 같다.
+
+1번부터 N번까지 N명의 사람이 원을 이루면서 앉아있고, 양의 정수 K(≤ N)가 주어진다. 이제 순서대로 K번째 사람을 제거한다.
+한 사람이 제거되면 남은 사람들로 이루어진 원을 따라 이 과정을 계속해 나간다. 이 과정은 N명의 사람이 모두 제거될 때까지 계속된다.
+원에서 사람들이 제거되는 순서를 (N, K)-조세퍼스 순열이라고 한다. 예를 들어 (7, 3)-조세퍼스 순열은 <3, 6, 2, 7, 5, 1, 4>이다.
+
+N과 K가 주어지면 (N, K)-조세퍼스 순열을 구하는 프로그램을 작성하시오.
+
+<입력>
+첫째 줄에 N과 K가 빈 칸을 사이에 두고 순서대로 주어진다. (1 ≤ K ≤ N ≤ 5,000)
+ 예제) 7 3
+<출력>
+ 예제) <3, 6, 2, 7, 5, 1, 4>
+"""
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+queue = []
+
+
+def del_third(list):
+    while len(list):
+        if len(list) > 2:
+            v = list[2]
+            del list[2]
+            print(v)
+            del_third(list)
+        else:
+            v = list[0]
+            del list[0]
+            print(v)
+            del_third(list)
+
+
+for i in range(n):
+    queue.append(i+1)
+
+del_third(queue)
+
+
+
+
+
