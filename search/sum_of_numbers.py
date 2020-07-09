8a1f0d882edb868bbb31654f7101213dc0bf4784
seon54 <seonhyeongpark54@gmail.com>
2020-07-09 21:09:58 +0900

    Update sum_of_numbers

--- search/sum_of_numbers.py
+++ search/sum_of_numbers.py
@@ -24,3 +24,30 @@ for i in range(0, n):
             break
 
 print(count)
+
+
+# 수업 내용
+
+n, m = map(int, input().split())
+l = list(map(int, input().split()))
+count = 0
+lt = 0
+rt = 1
+tot = l[0]
+
+while True:
+    if tot < m:
+        if rt < n:
+            tot += l[rt]
+            rt += 1
+        else:
+            break
+    elif tot == m:
+        count += 1
+        tot -= l[lt]
+        lt += 1
+    else:
+        tot -= l[lt]
+        lt += 1
+
+print(count)
